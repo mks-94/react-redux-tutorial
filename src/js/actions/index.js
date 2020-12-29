d0d2cdc827b3a07a06f4daebcda95935078870ac
mks-94 <muralikrishna9424.com>
2020-12-29 22:08:17 +0530

    Implemented proof of concept with simple redux

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,6 +1,6 @@
 import { ADD_ARTICLE } from "../constants/index";
 
-export function addArtcle(payload) {
+export function addArticle(payload) {
   return {
     type: ADD_ARTICLE,
     payload,
---
+++ src/js/components/List.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const List = ({ articles }) => {
+  return (
+    <ul>
+      {articles.map((el) => (
+        <li key={el.id}>{el.title}</li>
+      ))}
+    </ul>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return { articles: state.articles };
+};
+
+export default connect(mapStateToProps)(List);
