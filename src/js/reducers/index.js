e7c6001c91e2646cb86dadc41d8b8bb7ea735373
mks-94 <muralikrishna9424.com>
2020-12-31 20:12:08 +0530

    did nothing

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -6,10 +6,12 @@ const initialState = {
 
 function rootReducer(state = initialState, action) {
   if (action.type === ADD_ARTICLE) {
+    console.log(state, "Is state update here!");
     return Object.assign({}, state, {
       articles: state.articles.concat(action.payload),
     });
   }
+  console.log(state, "Is state updated here!");
   return state;
 }
 export default rootReducer;
